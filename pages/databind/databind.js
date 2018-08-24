30b2ea4c736e3658652cd8b34a77a37f2547ea64
zhengdandan <cha.baby@qq.com>
2018-08-24 15:25:29 +0800

    增加计算器

--- pages/databind/databind.js
+++ pages/databind/databind.js
@@ -4,6 +4,19 @@ Page({
       name: 'brave',
       age: 11
     },
-    content:"微信小程序绑定内容"
+    content:"微信小程序绑定内容",
+    users:[{
+      name:"fei",
+      age:36
+    },{
+      name:"juan",
+      age:30,
+    },{
+      name:"pan",
+      age:26
+    },{
+      name:"dan",
+      age:18
+    }]
   }
 })
